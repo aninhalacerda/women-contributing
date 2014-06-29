8f5c5d7421a3a68b1af778f83f34286cef0ae6fa
Raquel Guimaraes <raquelrag@gmail.com>
2014-06-28 22:29:18 -0300

    adds promises on script to close mongo connection and keep script from hanging

--- scripts/clean_database.js
+++ scripts/clean_database.js
@@ -1,5 +1,12 @@
 require ('../database')
 var User  = require('../models/user');
+var mongoose = require('mongoose');
+var Q = require('q');
+
+var deferred = Q.defer();
 User.remove({}, function (err) {
-  if (err) return handleError(err);
-}); 
+  if (err) deferred.reject(err);
+  else deferred.resolve();
+});
+
+deferred.promise.then(function() { mongoose.connection.close() });
--- scripts/import_users.js
+++ scripts/import_users.js
@@ -1,12 +1,22 @@
 require('../database');
+var mongoose = require('mongoose');
 var User = require('../models/user');
 var GithubRequest = require('./util/github_request');
+var Q = require('q');
 
 GithubRequest.get(1000).users(function (logins) {
+
+  var promises = [];
+
   logins.forEach(function (login) {
-    GithubRequest.getUser(login, function (user) {
-      var newUser = new User(user);
-      newUser.save();
-    });
+      var defered = Q.defer();
+      GithubRequest.getUser(login, function (user) {
+        var newUser = new User(user);
+        newUser.save();
+        defered.resolve(newUser);
+      });
+      promises.push(defered.promise);
   });
+
+  Q.all(promises).then(function() { mongoose.connection.close() });
 });
