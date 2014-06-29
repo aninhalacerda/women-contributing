aa502e7f34f3db39dde5c9a2648bf98a37e7a657
Raquel Guimaraes <raquelrag@gmail.com>
2014-06-28 22:47:53 -0300

    adds stdout.write as user feedback when retrieving/saving user data

--- scripts/import_users.js
+++ scripts/import_users.js
@@ -8,11 +8,15 @@ GithubRequest.get(1000).users(function (logins) {
 
   var promises = [];
 
+  process.stdout.write("Saving users ");
+
   logins.forEach(function (login) {
       var defered = Q.defer();
       GithubRequest.getUser(login, function (user) {
         var newUser = new User(user);
         newUser.save();
+
+        process.stdout.write(".");
         defered.resolve(newUser);
       });
       promises.push(defered.promise);
