199a261b1a8012707a21d1dc67f2e508c7a93772
Ana Lacerda <anaclara.lacerdas@gmail.com>
2015-01-07 15:38:34 -0300

    adding log on db error

--- database.js
+++ database.js
@@ -1,3 +1,5 @@
 var mongoose = require('mongoose');
 var env = process.env.NODE_ENV || 'development';
+var db = mongoose.connection;
 mongoose.connect('mongodb://localhost/women-contributing-'+env);
+db.on('error', console.error.bind(console, 'connection error:'));
