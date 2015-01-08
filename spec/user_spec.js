61a52e7eff66c0a591fc64caca7fa246e640b871
Ana Lacerda <anaclara.lacerdas@gmail.com>
2015-01-08 17:59:36 -0300

    Trying to fix tests -  still not good

--- spec/user_spec.js
+++ spec/user_spec.js
@@ -2,22 +2,25 @@ require ('../database');
 var User = require ('../models/user');
 
 beforeEach(function () {
-	User.remove({}, function() {
-	});
+    new User({"created_at" : "2014-01-01"}).save();
+    new User({"created_at" : "2014-02-01"}).save();
+    new User({"created_at" : "2014-03-01"}).save();
+});
+
+afterEach(function () {
+  User.remove({}, function() {
+    });
 });
 
 describe("UserTime", function() {
   it("should get user count grouped by time", function (done) {
-  	new User({"created_at" : "2014-01-01"}).save();
-  	new User({"created_at" : "2014-02-01"}).save();
-  	new User({"created_at" : "2014-03-01"}).save();
-  	User.byTime(function (users) {
-  		expect(users.length).toEqual(3);
-  		expect(users).toEqual([ { _id: { year: 2014, month: 1 }, count: 1 },
-														  { _id: { year: 2014, month: 2 }, count: 1 },
-														  { _id: { year: 2014, month: 3 }, count: 1 } ]);
-  		done();
-  	});
+    User.byTime(function (users) {
+      expect(users.length).toEqual(3);
+      expect(users).toEqual([ { _id: { year: 2014, month: 1 }, count: 1 },
+                              { _id: { year: 2014, month: 2 }, count: 1 },
+                              { _id: { year: 2014, month: 3 }, count: 1 } ]);
+      done();
+    });
   });
 });
 
@@ -26,9 +29,15 @@ describe("UserGender", function() {
     new User({"gender" : "female"}).save();
     new User({"gender" : "male"}).save();
     User.byGender(function (users) {
-      expect(users.length).toEqual(2);
-      expect(users).containsAll([ { _id:  "female", count: 1 },
-                              { _id:  "male" , count: 1 }]);
+      var captured = null;
+      try {
+        expect(users.length).toEqual(2);
+        expect(users).containsAll([ { _id:  "female", count: 1 },
+                                  { _id:  "male" , count: 1 }]);
+      } catch(e) {
+        captured = e;
+      }
+      expect(captured).toBe(null);
       done();
     });
   });
