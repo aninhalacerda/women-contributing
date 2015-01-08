99ac5af3b696884e514a0ca9d1b56f7f15a8dffb
Roselma Mendes <rmendes@thoughtworks.com>
2015-01-08 18:34:16 -0300

    Solve the problem that the mongo connection was not closed during the tests. Source: https://github.com/mhevery/jasmine-node/issues/241

--- spec/spec_helper.js
+++ spec/spec_helper.js
@@ -1,1 +1,11 @@
+var mongoose = require('mongoose');
+
 process.env.NODE_ENV = 'test';
+
+var _finishCallback = jasmine.Runner.prototype.finishCallback;
+jasmine.Runner.prototype.finishCallback = function () {
+    // Run the old finishCallback
+    _finishCallback.bind(this)();
+
+    mongoose.connection.close();
+};
