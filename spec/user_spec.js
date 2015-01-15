8e4fe2802f4767472b72babc004954d52795352f
Ana Lacerda <anaclara.lacerdas@gmail.com>
2015-01-15 11:35:13 -0300

    removing beforeEach anf creating specific users for each test

--- spec/user_spec.js
+++ spec/user_spec.js
@@ -1,18 +1,6 @@
 require ('../database');
 var User = require ('../models/user');
 
-beforeEach(function (done) {
-    new User({"created_at" : "2014-01-01", "gender" : "female"}).save(function() {
-      new User({"created_at" : "2014-02-01", "gender" : "female"}).save(function() {
-        new User({"created_at" : "2014-03-01", "gender" : "male"}).save(function() {
-          done();
-        });
-      });
-    });
-
-
-});
-
 afterEach(function (done) {
   User.remove({}, function() {
     done();
@@ -21,14 +9,20 @@ afterEach(function (done) {
 
 describe("UserTime", function() {
   it("should get user count grouped by time", function (done) {
-    User.byTime(function (users) {
-      expect(users.length).toEqual(3);
 
-      expect(users).toContain({ _id: { year: 2014, month: 1 }, count: 1 });
-      expect(users).toContain({ _id: { year: 2014, month: 2 }, count: 1 });
-      expect(users).toContain({ _id: { year: 2014, month: 3 }, count: 1 });
+    new User({"created_at" : "2014-01-01"}).save( function() {
+      new User({"created_at" : "2014-02-01"}).save( function() {
+        new User({"created_at" : "2014-03-01"}).save( function() {
 
-      done();
+          User.byTime(function (users) {
+            expect(users.length).toEqual(3);
+            expect(users).toContain({ _id: { year: 2014, month: 1 }, count: 1 });
+            expect(users).toContain({ _id: { year: 2014, month: 2 }, count: 1 });
+            expect(users).toContain({ _id: { year: 2014, month: 3 }, count: 1 });
+            done();
+          });
+        });
+      });
     });
   });
 });
@@ -38,23 +32,16 @@ describe("UserGender", function() {
 
     new User({"gender" : "female"}).save(function() {
       new User({"gender" : "male"}).save(function() {
-        User.byGender(function (users) {
-          var captured = null;
-          try {
+        new User({"gender" : "male"}).save(function() {
+
+          User.byGender(function (users) {
             expect(users.length).toEqual(2);
-
-            expect(users).toContain({ _id: "female", count: 3 });
+            expect(users).toContain({ _id: "female", count: 1 });
             expect(users).toContain({ _id: "male", count: 2 });
-        } catch(e) {
-          captured = e;
-        }
-        expect(captured).toBe(null);
-        done();
-      });
-
+            done();
+          });
+        });
       });
     });
-
-
   });
 });
