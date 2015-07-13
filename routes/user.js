747920ea205a6ac98ce079a7ea2d131f0a70d856
Ana Lacerda <anaclara.lacerdas@gmail.com>
2015-07-12 22:17:54 -0300

    Removing call to deprecated function

--- routes/user.js
+++ routes/user.js
@@ -2,12 +2,12 @@ var User = require('../models/user');
       
 exports.byTime = function(req, res) {
   User.byTime(function (users) {
-    res.json(200, { users: users });
+    res.status(200).json({ users: users });
   });
 }
 
 exports.byGender = function(req, res) {
 	User.byGender(function (users) {
-		res.json(200, { users: users });
+    res.status(200).json({ users: users });
 	});
 }
