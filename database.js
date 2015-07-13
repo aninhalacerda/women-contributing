6c84aacca247c01a962931f6128c73ba7ece11eb
Ana Lacerda <anaclara.lacerdas@gmail.com>
2015-07-12 21:00:00 -0300

    #24: pointing mongolab url to config property

--- database.js
+++ database.js
@@ -1,8 +1,6 @@
 var mongoose = require('mongoose');
 var env = process.env.NODE_ENV || 'development';
 var db = mongoose.connection;
-// mongodb://heroku_sfvgxnp9:r0tbpfh6astnkl6snqtke7q3kv@ds047802.mongolab.com:47802/heroku_sfvgxnp9
-// var connection_url = process.env.DATABASE_URL || 'mongodb://localhost/women-contributing-'+env;
-var connection_url = 'mongodb://heroku_sfvgxnp9:r0tbpfh6astnkl6snqtke7q3kv@ds047802.mongolab.com:47802/heroku_sfvgxnp9';
+var connection_url = process.env.MONGOLAB_URI || 'mongodb://localhost/women-contributing-'+env;
 mongoose.connect(connection_url);
 db.on('error', console.error.bind(console, 'connection error:'));
