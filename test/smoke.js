399ff94455cc60f097a2edd4e179fd8ab268cd29
Ana Lacerda <anaclara.lacerdas@gmail.com>
2015-07-12 22:42:17 -0300

    ping tests for API endpoint on heroku

---
+++ test/smoke.js
@@ -0,0 +1,34 @@
+var request = require('superagent');
+var expect = require('expect.js');
+var should = require('should');
+var app = require('../app');
+var server;
+var URL = 'https://women-contributing.herokuapp.com/';
+  
+describe('Production API', function(){
+	it ('should be alive', function(done){
+    request.get(URL).end(function(err, res){
+	    expect(res).to.exist;
+	    expect(res.status).to.equal(200);
+	    expect(res.text).to.contain('Express');
+	    done();
+    });
+  });
+  it ('should get all users by gender', function(done){
+    request.get(URL+'users/gender').end(function(err, res){
+	    expect(res).to.exist;
+	    expect(res.status).to.equal(200);
+	    res.body.should.have.property('users');
+	    done();
+    });
+  });
+
+ it ('should get all users by time', function(done){
+    request.get(URL+'users/time').end(function(err, res){
+	    expect(res).to.exist;
+	    expect(res.status).to.equal(200);
+	    res.body.should.have.property('users');
+	    done();
+    });
+  });
+});
