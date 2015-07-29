69451fc4526605f41bb06c88028f17da9a16e43d
Ana Lacerda <anaclara.lacerdas@gmail.com>
2015-07-29 19:07:34 -0300

    #21: catching error when genederize is on limit of requests per day

--- scripts/update_user_gender.js
+++ scripts/update_user_gender.js
@@ -8,40 +8,41 @@ function genderize(numUsers) {
   var streamDeferred = Q.defer();
   var promises = [streamDeferred.promise];
   var usersUpdated = 0;
-  var stream = User.find({"gender": null}).limit(numUsers).stream();
+  var stream = User.find({"gender": null}).and({"name": {$ne : null}}).limit(numUsers).stream();
 
   stream.on('data', function (user) {
-    if (user.name != undefined) {
-      var deferred = Q.defer();
-      GenderizeRequest.genderize(user.name, function(gender) {
-        updateUserGender(user.login, gender, function(err, numAffected) {
-          if (err != null) {
-            console.log("Could not update user gender");
-            console.log(err);
-          } else {
-            deferred.resolve(numAffected);
-            if (++usersUpdated == numUsers) {streamDeferred.resolve()};
-          }
-        });
+    var deferred = Q.defer();
+    GenderizeRequest.genderize(user.name, function(gender) {
+      updateUserGender(user.login, gender, function(err, numAffected) {
+        if (err != null) {
+          console.log("Could not update user gender");
+          console.log(err);
+        } else {
+          process.stdout.write(".");
+          deferred.resolve(numAffected);
+          if (++usersUpdated == numUsers) {streamDeferred.resolve()};
+        }
       });
-      promises.push(deferred.promise);
-    } else {
-      usersUpdated++;
-    }
+    });
+    promises.push(deferred.promise);
   });
 
   Q.all(promises).then(function() { mongoose.connection.close(); });
 };
 
 function updateUserGender(login, gender, callback) {
-  gender_probability = gender.probability || 0;
-  User.update({login: login},
-              {$set :
-                {gender: gender.gender,
-                  gender_probability: gender_probability}
-              },
-              {},
-              callback);
+  if (gender.gender != undefined) {
+    gender_probability = gender.probability || 0;
+    User.update({login: login},
+                {$set :
+                  {gender: gender.gender,
+                    gender_probability: gender_probability}
+                },
+                {},
+                callback);
+  } else {
+    console.log("Genderize response: undefined for user " + login);
+  }
 }
 
-genderize(5);
+genderize(100);
