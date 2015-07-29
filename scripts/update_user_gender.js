000e88024ac1193445ced25ba84538e4ddeee214
Ana Lacerda <anaclara.lacerdas@gmail.com>
2015-07-29 17:59:00 -0300

    WIP: adding promises to update gender script

--- scripts/update_user_gender.js
+++ scripts/update_user_gender.js
@@ -1,22 +1,36 @@
 require ('../database');
-var User = require ('../models/user');
 var mongoose = require('mongoose');
+var User = require ('../models/user');
 var GenderizeRequest = require('./util/genderize_request');
+var Q = require('q');
 
-var stream = User.find().limit(20).stream();
-stream.on('data', function (user) {
-  if (user.name != undefined) {
-    GenderizeRequest.genderize(user.name, function(gender) {
-      updateUserGender(user.login, gender);
-    });
-  }
-});
+function genderize() {
+  var stream = User.find({"gender": null}).limit(5).stream();
+  var promises = [];
+  var users = ["nate", "canadaduane", "nicksieger"];
 
-stream.on('end', function () {
-  mongoose.connection.close();
-});
+  users.forEach(function(user) {
+        var deferred = Q.defer();
 
-function updateUserGender(login, gender) {
+        GenderizeRequest.genderize("Luke bla", function(gender) {
+          updateUserGender(user, gender, function(err, numAffected) {
+                  if (err != null) {
+                    console.log("Could not update user gender");
+                    console.log(err);
+                  } else {
+                    console.log("Deferred!");
+                    deferred.resolve(numAffected);
+                  }
+                });
+          process.stdout.write(". " + user + ". ");
+       });
+       promises.push(deferred.promise);
+  });
+
+  Q.all(promises).then(function() { console.log("finished!"); mongoose.connection.close() });
+};
+
+function updateUserGender(login, gender, callback) {
   gender_probability = gender.probability || 0;
   User.update({login: login},
               {$set :
@@ -27,9 +41,4 @@ function updateUserGender(login, gender) {
               callback);
 }
 
-function callback(err, numAffected) {
-  if (err != null) {
-    console.log("Could not update user gender");
-    console.log(err);
-  }
-}
+genderize();
